f3fe3a8dc0224a0fd21948ccf0843770b4017242
Gary Villegas <gary.villegasb@gmail.com>
2024-09-26 20:42:01 -0300

    se configura pantalla de alumno

--- asistenciaqr/src/app/home/home.page.ts
+++ asistenciaqr/src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  isMobile: boolean = false;
 
-  constructor() {}
+  constructor(private platform: Platform) {}
 
+  ngOnInit() {
+    this.isMobile =
+      this.platform.is('mobile') ||
+      this.platform.is('tablet') ||
+      this.isScreenSmall();
+  }
+
+  private isScreenSmall(): boolean {
+    return window.innerWidth < 768;
+  }
 }
