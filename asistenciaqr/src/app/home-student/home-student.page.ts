91e8e0dd0fdf79fd9c7426ed1caa328376b88121
Gary Villegas <gary.villegasb@gmail.com>
2024-10-20 13:28:45 -0300

    Se agrega generador QR en vista de profesor

--- asistenciaqr/src/app/home-student/home-student.page.ts
+++ asistenciaqr/src/app/home-student/home-student.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Platform, IonicModule } from '@ionic/angular';
+import { Html5QrcodeScanner, Html5QrcodeScanType } from 'html5-qrcode';
 
 @Component({
   selector: 'app-home-student',
@@ -10,6 +11,10 @@ export class HomeStudentPage implements OnInit {
   isMobile: boolean = true;
   isModalCredential: boolean = false;
   isModalSettings: boolean = false;
+  isModalScanner: boolean = false;
+  scannerResult: string | null = null;
+  private html5QrCode: Html5QrcodeScanner | null = null;
+  isCameraPermission: boolean = false;
 
   constructor(private platform: Platform) {}
 
@@ -25,11 +30,55 @@ export class HomeStudentPage implements OnInit {
     return window.innerWidth < 768;
   }
 
+  setScannerOpen(isOpen: boolean) {
+    this.isModalScanner = isOpen;
+  }
+
   setCredentialOpen(isOpen: boolean) {
     this.isModalCredential = isOpen;
   }
 
   setSettingsOpen(isOpen: boolean) {
     this.isModalSettings = isOpen;
+  }
+
+  requestPermisssion() {
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then((strem) => {
+          this.isCameraPermission = true;
+          this.startScanner();
+        })
+        .catch((error) => {
+          alert('Error al solicitar permisos de camará');
+        });
+    } else {
+      alert('Navegador no soporta el acceso a camará');
+    }
+  }
+
+  startScanner() {
+    const config = {
+      fps: 10,
+      qrbox: 250,
+      supportedScanTypes: [Html5QrcodeScanType.SCAN_TYPE_CAMERA],
+    };
+    this.html5QrCode = new Html5QrcodeScanner('reader', config, false);
+    this.html5QrCode.render(
+      (result) => {
+        this.scannerResult = result;
+        console.log('resultado del scanner', result);
+      },
+      (error) => {
+        console.warn('error al escanear codigo QR', error);
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.html5QrCode) {
+      this.html5QrCode.clear();
+    }
   }
 }
