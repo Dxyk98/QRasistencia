3c4039dcb7c5ccb528c767147e79b8855b6d3795
Gary Villegas <gary.villegasb@gmail.com>
2024-10-25 16:52:51 -0300

    Se agrega servicio de storage, falta saber ocuparlo en el front

--- asistenciaqr/src/app/administrador/administrador.page.ts
+++ asistenciaqr/src/app/administrador/administrador.page.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../storage.service';
+
+interface Persona {
+  name: string;
+  age: string;
+  carrera: string;
+  identificador: string;
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-administrador',
@@ -6,10 +16,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./administrador.page.scss'],
 })
 export class AdministradorPage implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
