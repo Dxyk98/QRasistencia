1cd50b76ee1bf33c1c5f29c609d89c013811f07d
Gary Villegas <gary.villegasb@gmail.com>
2024-11-19 14:37:16 -0300

    Se crean clases desde profesor

--- asistenciaqr/src/app/profesor-home/profesor-home.module.ts
+++ asistenciaqr/src/app/profesor-home/profesor-home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ProfesorHomePageRoutingModule } from './profesor-home-routing.module';
 import { ProfesorHomePage } from './profesor-home.page';
@@ -13,6 +13,7 @@ import { QRCodeModule } from 'angularx-qrcode';
     IonicModule,
     ProfesorHomePageRoutingModule,
     QRCodeModule,
+    ReactiveFormsModule,
   ],
   declarations: [ProfesorHomePage],
 })
--- asistenciaqr/src/app/profesor-home/profesor-home.page.ts
+++ asistenciaqr/src/app/profesor-home/profesor-home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../storage.service';
 import { ToastController } from '@ionic/angular';
 import { UserService } from '../user.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-profesor-home',
@@ -16,18 +17,43 @@ export class ProfesorHomePage implements OnInit {
   claseHoy: any; // Propiedad para almacenar la clase de hoy
   usuario: any; // Store user data here
   selectedProfesorId: string = ''; // Store the selected professor ID
+  claseForm: FormGroup;
+  personaSeleccionada: Persona | null = null;
+  personas: Persona[] = [];
+  carreras: string[] = [
+    'Ingeniería Informatica',
+    'Tecnico Informatico',
+    'Medicina',
+    'Derecho',
+    'Arquitectura',
+    'Administración',
+  ];
+  horario: string[] = ['Diurno', 'Vespertino'];
+  diaSemana: string[] = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes'];
 
   constructor(
     private storageService: StorageService,
     private toastController: ToastController,
-    private userService: UserService
-  ) {}
+    private userService: UserService,
+    private formBuilder: FormBuilder
+  ) {
+    this.claseForm = this.formBuilder.group({
+      nombre: ['', [Validators.required, Validators.minLength(2)]],
+      carreraClase: ['', Validators.required],
+      horaInicio: ['', Validators.required],
+      horaTermino: ['', Validators.required],
+      diurnoVespertino: ['', Validators.required],
+      dias: ['', Validators.required],
+      profesor: ['', Validators.required],
+    });
+  }
 
   async ngOnInit() {
     await this.storageService.init(); //se llama al servicio publico
     this.checkIfMobile();
     window.addEventListener('resize', () => this.checkIfMobile());
     this.cargarClases();
+    this.cargarPersonas();
     this.usuario = await this.userService.obtenerUsuario(); // Fetch logged-in user
     this.selectedProfesorId = this.usuario.id; // Assign the professor's ID
   }
@@ -50,6 +76,12 @@ export class ProfesorHomePage implements OnInit {
     }
   }
 
+  //Modal crear clase
+  isCrearModalOpen = false;
+  setOpenCrear(isOpen: boolean) {
+    this.isCrearModalOpen = isOpen;
+  }
+
   // Modal QR
   isModalOpen = false;
   setOpen(isOpen: boolean) {
@@ -63,6 +95,45 @@ export class ProfesorHomePage implements OnInit {
     } catch (error) {
       console.error('Error al cargar clases:', error);
       await this.mostrarMensaje('Error al cargar las clases');
+    }
+  }
+
+  async guardarClase() {
+    if (this.claseForm.valid) {
+      const clase = this.claseForm.value;
+      try {
+        const nuevaClase = await this.storageService.agregarClase(clase);
+        this.mostrarMensaje(`Clase guardada con éxito. ID: ${nuevaClase.id}`);
+        this.claseForm.reset();
+        this.cargarClases();
+      } catch (error) {
+        this.mostrarMensaje('Error al guardar la clase');
+        console.error(error);
+      }
+    } else {
+      this.mostrarMensaje('Por favor, complete todos los campos correctamente');
+    }
+  }
+
+  //funcion para eliminar clase
+  async eliminarClase(id: string) {
+    try {
+      await this.storageService.eliminarClase(id);
+      this.mostrarMensaje('Clase eliminada con éxito');
+      this.cargarClases();
+    } catch (error) {
+      this.mostrarMensaje('Error al eliminar la clase');
+      console.error(error);
+    }
+  }
+
+  async cargarPersonas() {
+    try {
+      this.personas = await this.storageService.obtenerDatos('personas');
+      console.log(this.personas);
+    } catch (error) {
+      console.error('Error al cargar personas', error);
+      this.mostrarMensaje('Error al cargar personas');
     }
   }
 
@@ -87,4 +158,15 @@ interface Clase {
     id: string;
     nombre: string;
   };
+}
+
+//interface persona
+interface Persona {
+  identificador: string;
+  nombre: string;
+  apellido: string;
+  carrera: string;
+  tipoPersona: string;
+  email: string;
+  contrasena: string;
 }
